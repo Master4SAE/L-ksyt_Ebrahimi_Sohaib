3d4bc6a4e300099df32b56b47aebdd8089762220
sohaibebrahimi <ebrahimi_sa@icloud.com>
2022-08-22 14:47:25 +0300

    tehtävä 2.4 tehty

---
+++ tehtävä2.4.py
@@ -0,0 +1,9 @@
+a = float(input('anna ensimmäinen kokonaisluku '))
+b = float(input('anna toinen kokonaisluku '))
+c = float(input('anna  kolmas kokonaisluku '))
+
+summa = a+b+c
+tulo = a*b*c
+keski = (a+b+c)/3
+
+print(f'kokonaislukujen summa on {summa} niiden tulo on {tulo} ja keski-arvo on {keski} ')

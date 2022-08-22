0de3c604b00461119c8c30a065164ee4a435ab69
sohaibebrahimi <ebrahimi_sa@icloud.com>
2022-08-22 14:38:40 +0300

    tehtävä 2.3 tehty

---
+++ tehtävä2.3.py
@@ -0,0 +1,5 @@
+kanta = float(input("anna kanta"))
+korkeus = float(input("anna korkeus"))
+pinta_ala = kanta * korkeus
+piiri = 2*kanta + 2*korkeus
+print(f'pinta-ala on {pinta_ala} ja piiri on {piiri}')

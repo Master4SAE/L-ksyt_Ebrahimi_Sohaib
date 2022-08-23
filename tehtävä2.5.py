3d6cacc0c7fa1d1a0770ca46adae4a257b0cb953
sohaibebrahimi <ebrahimi_sa@icloud.com>
2022-08-23 22:01:01 +0300

    tehty2.5

--- tehtävä2.5.py
+++ tehtävä2.5.py
@@ -7,8 +7,7 @@ ans1 = (leivika*20)*32*13.3
 ans2 = (naula*32)*13.3
 ans3 = luoti*13.3
 grammas = ans2+ans3+ans1
-k_g = math.floor(grammas/1000)
-left_overs =k_g-grammas
+k_g = int(grammas/1000)
+left_overs =grammas % 1000
 
-print(math.floor(grammas/1000)-grammas)
 print(f'{k_g} kilo ja {left_overs} gramma')

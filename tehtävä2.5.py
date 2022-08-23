aaf2a254cb85bf9ce4d3eba8bb5e8ecb2ffd08e5
sohaibebrahimi <ebrahimi_sa@icloud.com>
2022-08-23 17:59:38 +0300

    kesken

--- tehtävä2.5.py
+++ tehtävä2.5.py
@@ -6,7 +6,9 @@ luoti = float(input("anna mitta luotina"))
 ans1 = (leivika*20)*32*13.3
 ans2 = (naula*32)*13.3
 ans3 = luoti*13.3
-ans4 = ans2+ans3+ans1
-ans5 = math.floor(ans4/1000)
-ans6 = ans4 - ans5
-print(f'{ans5} kilo ja {ans6} gramma')
+grammas = ans2+ans3+ans1
+k_g = math.floor(grammas/1000)
+left_overs =k_g-grammas
+
+print(math.floor(grammas/1000)-grammas)
+print(f'{k_g} kilo ja {left_overs} gramma')

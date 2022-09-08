2a4776a0397cfc6d0d6c11da2ee0135cbdb7a8a7
sohaibebrahimi <ebrahimi_sa@icloud.com>
2022-09-08 15:54:54 +0300

    tehty

--- 4.5.py
+++ 4.5.py
@@ -1,1 +1,10 @@
-hi
+user_name = 'SAE'
+pass_word = 'EAS'
+
+give_username = input(" hey give me your username")
+give_password = input(" hey give me your password")
+
+while user_name != give_username or pass_word != give_password and pass_word != give_password:
+    print("hi")
+else:
+    print("toimi")

06e9cbccbd2b4e8aff1a399dfb596f49397236e2
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-08 18:54:32 +0300

    Delete tehtävä2.4.py

--- tehtävä2.4.py
+++
@@ -1,9 +0,0 @@
-a = float(input('anna ensimmäinen kokonaisluku '))
-b = float(input('anna toinen kokonaisluku '))
-c = float(input('anna  kolmas kokonaisluku '))
-
-summa = a+b+c
-tulo = a*b*c
-keski = (a+b+c)/3
-
-print(f'kokonaislukujen summa on {summa} niiden tulo on {tulo} ja keski-arvo on {keski} ')

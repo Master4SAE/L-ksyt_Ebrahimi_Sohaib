8fb2ed95b1c998cf91495d224e6474a0411f856e
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-08 18:54:20 +0300

    Delete tehtävä2.3.py

--- tehtävä2.3.py
+++
@@ -1,5 +0,0 @@
-kanta = float(input("anna kanta"))
-korkeus = float(input("anna korkeus"))
-pinta_ala = kanta * korkeus
-piiri = 2*kanta + 2*korkeus
-print(f'pinta-ala on {pinta_ala} ja piiri on {piiri}')

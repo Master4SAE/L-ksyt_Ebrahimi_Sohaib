dc4b3180b1e577a6fd90c344ce0d6c21b724c620
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-12 19:01:33 +0300

    Update 3. Conditional structures (if).py

--- 3. Conditional structures (if).py
+++ 3. Conditional structures (if).py
@@ -52,3 +52,16 @@ elif suku_puoli == 'mies' and hemoglobin_arvo > 195:
     print("korkea")
 else:
     print(" ohjelma ei toimi !! ")
+
+#3.4
+
+vuosi_luku = int(input("Hei anna vuosiluku"))
+
+if vuosi_luku % 4 == 0:
+    print("vuosi on karkausvuosi")
+elif vuosi_luku % 4 != 0:
+    print("ei ole karkausvuosi")
+elif vuosi_luku % 100 == 0 and vuosi_luku % 400 == 0:
+    print("vuosi on karkausvuosi")
+else:
+    print("vuosi ei ole karkaus vuosi")

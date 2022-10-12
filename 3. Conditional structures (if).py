e6dd260c906bfcec25a94b1fe80e15585d296e7d
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-12 18:58:45 +0300

    Update 3. Conditional structures (if).py

--- 3. Conditional structures (if).py
+++ 3. Conditional structures (if).py
@@ -1,1 +1,12 @@
+#3.1
+
+kuhan_length = float(input(' Hei anna kuhan pituuden senttimetreinä '))
+How_much_cm_is_left = 37- kuhan_length
+if kuhan_length >= 37:
+    print(" Toimi !")
+else:
+    print(f"laske takaisin järveen {How_much_cm_is_left} "
+          f"senttiä alimmasta sallitusta pyyntimitasta puuttuu !")
+    
+#3.2
 

710a2fb5e40da314d240c9e26f8bff8eea73b24f
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-12 19:00:32 +0300

    Update 3. Conditional structures (if).py

--- 3. Conditional structures (if).py
+++ 3. Conditional structures (if).py
@@ -30,3 +30,25 @@ elif hytti_luokka == c:
     print(" C on ikkunaton hytti autokannen alapuolella. ")
 else:
     print("  Virheellinen hyttiluokka")
+    
+#3.3
+
+suku_puoli = input(" Hei oletko mies tai nainen ? ").lower()
+hemoglobin_arvo = float(input(" Anna hemoglobiiniarvo (g/l). " ))
+
+#nainen
+if (suku_puoli == 'nainen' and 117 <= hemoglobin_arvo <= 175):
+    print("normaali")
+elif suku_puoli == 'nainen' and hemoglobin_arvo < 117:
+    print("alhainen")
+elif suku_puoli == 'nainen' and hemoglobin_arvo > 175:
+    print("korkea")
+#mies
+elif (suku_puoli == 'mies' and 134 <= hemoglobin_arvo <= 195):
+    print("normaali")
+elif suku_puoli == 'mies' and hemoglobin_arvo < 134:
+    print("alhainen")
+elif suku_puoli == 'mies' and hemoglobin_arvo > 195:
+    print("korkea")
+else:
+    print(" ohjelma ei toimi !! ")

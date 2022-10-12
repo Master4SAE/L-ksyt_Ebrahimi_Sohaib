c7c03d3cbd145497b7eea874b1db5aafa3ce7057
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-12 18:57:47 +0300

    Delete 3.1.py

--- 3.1.py
+++
@@ -1,7 +0,0 @@
-kuhan_length = float(input(' Hei anna kuhan pituuden senttimetreinä '))
-How_much_cm_is_left = 37- kuhan_length
-if kuhan_length >= 37:
-    print(" Toimi !")
-else:
-    print(f"laske takaisin järveen {How_much_cm_is_left} "
-          f"senttiä alimmasta sallitusta pyyntimitasta puuttuu !")

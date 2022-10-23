3fd6e782ccf86f1645f20bb5490ea85d31d55278
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-24 00:35:02 +0300

    Update 5. List structures and iterative loops (for).py

--- 5. List structures and iterative loops (for).py
+++ 5. List structures and iterative loops (for).py
@@ -52,3 +52,19 @@ if user_input > 1:
         print(user_input, "is a prime number")
 else:
     print("The number you gave was not prime")
+
+#5.4
+#print("Write a program that asks the user to enter the names of five cities one by on "
+#      "(use a for loop for reading the names) and stores them into a list structure."
+#      " Finally, the program prints out the names of the cities one by one,"
+#      " one city per line, in the same order they were read as input."
+#      " Use a for loop for asking the names and a for/in loop to iterate through the list.")
+city_names = []
+
+for i in range(0, 5):
+    print("enter the names of five cities one by one")
+    user_input = input("City ? > ")
+    city_names.append(user_input)
+
+for city in city_names:
+    print(city)

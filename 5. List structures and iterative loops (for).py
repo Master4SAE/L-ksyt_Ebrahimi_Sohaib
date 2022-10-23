a2ba37b97b6fdb4a1875617008012ecc361f9c05
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-23 23:25:57 +0300

    Update 5. List structures and iterative loops (for).py

--- 5. List structures and iterative loops (for).py
+++ 5. List structures and iterative loops (for).py
@@ -14,3 +14,20 @@ for every_time in rolling_dice:
       print(dice)
       my_rolled_dices.append(dice)
 print(f"the sum of all dices are {sum(my_rolled_dices)}")
+
+#5.2
+print("2.Write a program that asks the user to enter numbers until they input an empty string to quit."
+      " At the end, the program prints out the five greatest numbers sorted in descending order."
+      " Hint: You can reverse the order of sorted list items by using the sort method with the reverse=True argument.")
+
+number_list = []
+
+user_input = input("Hey enter numbers !\n For quiting press Enter\n> ")
+
+while user_input != "":
+    number_list.append(int(user_input))
+    user_input = input("Hey enter numbers !\n For quiting press Enter\n> ")
+
+number_list.sort(reverse=True)
+
+print(number_list[0:5])

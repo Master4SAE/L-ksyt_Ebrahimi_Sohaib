a7f0eb7eaae633a8776baa357c768dc96a60bfbf
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2022-10-25 09:46:04 +0300

    Update 6. Functions.py

--- 6. Functions.py
+++ 6. Functions.py
@@ -14,3 +14,23 @@ while True:
     count = count + 1
     if dice_rolled == 6:
         break
+      
+#6.2
+import random
+
+
+def get_dice():
+    my_dice = random.randint(1, sides)
+    return my_dice
+
+
+sides = int(input("Hey how many sides should the dice have?\n > "))
+count = 1
+
+while True:
+    dice_rolled = get_dice()
+    print(f"Its the {count} round and the dice rolled number is {dice_rolled}")
+    count = count + 1
+    if dice_rolled == sides:
+        break
+

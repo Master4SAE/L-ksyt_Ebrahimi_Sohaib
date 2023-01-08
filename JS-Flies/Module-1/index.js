6358035574ce3daa45e9f31e9d1aafde16c76a1c
sohaibebrahimi <ebrahimi_sa@icloud.com>
2023-01-08 17:01:23 +0200

    task-3 done

--- JS-Flies/Module-1/index.js
+++ JS-Flies/Module-1/index.js
@@ -4,3 +4,23 @@ console.log("I'am printing to console!")
 const name = prompt("whats your name user?");
 const greeting = `Hello ${name}`;
 document.querySelector("#greeting").innerHTML= greeting
+
+const num1 = prompt("give me number");
+const num2 = prompt("give me number");
+const num3 = prompt("give me number");
+const numOne = parseInt(num1);
+const numTwo = parseInt(num2);
+const numRd = parseInt(num3);
+const sum = numOne + numTwo + numRd;
+
+document.querySelector("#sum").innerHTML= "The sum of numbers are" + sum;
+
+const average = (numOne + numTwo + numRd) / 3;
+
+document.querySelector("#avre").innerHTML= "The the average of numbers are" + average;
+
+const product = (numOne * numTwo * numRd);
+
+document.querySelector("#task-3").innerHTML= "The product of numbers are" + product;
+
+
